ecdd62b7145c215aac6f40ba9efd1a4ca0dd07bf
Elizaveta Zabrodskaya <e.bagrintseva@gmail.com>
2021-06-17 16:44:05 +0300

    Добавляем анимацию

--- current-time/index.js
+++ current-time/index.js
@@ -1,59 +1,59 @@
 `use strict`;
 
 function showDate(deadline) {
-  let dateNow = new Date();
-  let dateStop = new Date(deadline);
-  let timeRemaining = (dateStop.getTime() - dateNow.getTime()) / 1000;
+	const dateNow = new Date();
+	const dateStop = new Date(deadline);
+	const timeRemaining = (dateStop.getTime() - dateNow.getTime()) / 1000;
 
-  function hello() {
-    let currentHour = dateNow.getHours();
+	function hello() {
+		const currentHour = dateNow.getHours();
 
-    if (currentHour < 6) {
-      return `Добрый день!`;
-    } else if (currentHour < 12) {
-      return `Доброе утро!`;
-    } else if (currentHour < 19) {
-      return `Добрый день!`;
-    } else {
-      return `Добрый вечер!`;
-    }
-  }
+		if (currentHour < 6) {
+			return `Добрый день!`;
+		} else if (currentHour < 12) {
+			return `Доброе утро!`;
+		} else if (currentHour < 19) {
+			return `Добрый день!`;
+		} else {
+			return `Добрый вечер!`;
+		}
+	}
 
-  const daysName = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+	const daysName = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-  function addZero(time) {
-    if (time < 10) {
-      return `0${time}`;
-    } else {
-      return time;
-    }
-  }
+	function addZero(time) {
+		if (time < 10) {
+			return `0${time}`;
+		} else {
+			return time;
+		}
+	}
 
-  function time() {
-    let hours = dateNow.getHours();
-    let ampm = hours >= 12 ? `PM` : `AM`;
-    hours = hours % 12;
-    hours = hours ? hours : 12;
+	function time() {
+		let hours = dateNow.getHours();
+		const ampm = hours >= 12 ? `PM` : `AM`;
+		hours %= 12;
+		hours = hours ? hours : 12;
 
-    return `${addZero(hours)}:${addZero(dateNow.getMinutes())}:${addZero(dateNow.getSeconds())} ${ampm}`;
-  }
+		return `${addZero(hours)}:${addZero(dateNow.getMinutes())}:${addZero(dateNow.getSeconds())} ${ampm}`;
+	}
 
-  function days() {
-    if (timeRemaining > 0) {
-      let dayRemaining = Math.floor(timeRemaining / 60 / 60 / 24);
-      let daysCount = `дней`;
-      if (dayRemaining == 1) {
-        daysCount = `день`
-      } else if (dayRemaining < 5 && dayRemaining > 1) {
-        daysCount = `дня`;
-      }
-      return `${dayRemaining} ${daysCount}`
-    } else {
-      return `0 дней`
-    }
-  }
+	function days() {
+		if (timeRemaining > 0) {
+			const dayRemaining = Math.floor(timeRemaining / 60 / 60 / 24);
+			let daysCount = `дней`;
+			if (dayRemaining === 1) {
+				daysCount = `день`;
+			} else if (dayRemaining < 5 && dayRemaining > 1) {
+				daysCount = `дня`;
+			}
+			return `${dayRemaining} ${daysCount}`;
+		} else {
+			return `0 дней`;
+		}
+	}
 
-  return `${hello()}
+	return `${hello()}
   Сегодня: ${daysName[dateNow.getDay() - 1]}
   Текущее время: ${time()}
   До нового года осталось ${days()}`;
@@ -65,11 +65,11 @@ div.style.cssText = `
 text-align: center;
 font-size: 30px;
 margin-top: 10%;
-`
+`;
 
 setInterval(() => {
-  div.innerText = showDate(`31 dec 2021`);
-}, 1000)
+	div.innerText = showDate(`31 dec 2021`);
+}, 1000);
 
 
 document.body.append(div);
--- js/script.js
+++ js/script.js
@@ -1,53 +1,124 @@
-window.addEventListener(`DOMContentLoaded`, function () {
-  `use strict`;
+window.addEventListener(`DOMContentLoaded`, () => {
+	`use strict`;
 
-  function countTimer(deadline) {
-    const timerHours = document.querySelector(`#timer-hours`),
-      timerMinutes = document.querySelector(`#timer-minutes`),
-      timerSeconds = document.querySelector(`#timer-seconds`);
+	// timer
+	const countTimer = deadline => {
+		const timerHours = document.querySelector(`#timer-hours`),
+			timerMinutes = document.querySelector(`#timer-minutes`),
+			timerSeconds = document.querySelector(`#timer-seconds`);
 
-    function getTimeRemaining() {
-      let dateStop = new Date(deadline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = Math.floor(timeRemaining % 60),
-        minutes = Math.floor((timeRemaining / 60) % 60),
-        hours = Math.floor(timeRemaining / 60 / 60);
+		function getTimeRemaining() {
+			const dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining / 60 / 60);
 
-      return { timeRemaining, hours, minutes, seconds };
-    }
+			return { timeRemaining, hours, minutes, seconds };
+		}
 
-    function addZero(time) {
-      if (time < 10) {
-        return `0${time}`;
-      } else {
-        return time;
-      }
-    }
+		function addZero(time) {
+			if (time < 10) {
+				return `0${time}`;
+			} else {
+				return time;
+			}
+		}
 
-    function updateClock() {
-      let timer = getTimeRemaining();
+		function updateClock() {
+			const timer = getTimeRemaining();
 
-      timerHours.textContent = addZero(timer.hours);
-      timerMinutes.textContent = addZero(timer.minutes);
-      timerSeconds.textContent = addZero(timer.seconds);
+			timerHours.textContent = addZero(timer.hours);
+			timerMinutes.textContent = addZero(timer.minutes);
+			timerSeconds.textContent = addZero(timer.seconds);
 
-      if (timer.timeRemaining > 0) {
-        setInterval(updateClock, 1000);
-      } else {
-        timerHours.textContent = `00`;
-        timerHours.style.color = `red`;
-        timerMinutes.textContent = `00`;
-        timerMinutes.style.color = `red`;
-        timerSeconds.textContent = `00`;
-        timerSeconds.style.color = `red`;
-      }
-    }
+			if (timer.timeRemaining > 0) {
+				setInterval(updateClock, 1000);
+			} else {
+				timerHours.textContent = `00`;
+				timerHours.style.color = `red`;
+				timerMinutes.textContent = `00`;
+				timerMinutes.style.color = `red`;
+				timerSeconds.textContent = `00`;
+				timerSeconds.style.color = `red`;
+			}
+		}
 
-    updateClock();
-  }
+		updateClock();
+	};
+	countTimer('18 june 2021');
 
-  countTimer('17 june 2021')
+	// menu
+	const toggleMenu = () => {
+		const btnMenu = document.querySelector(`.menu`),
+			menu = document.querySelector(`menu`),
+			closeBtn = document.querySelector(`.close-btn`),
+			menuItems = menu.querySelectorAll(`ul>li`);
 
+		const handlerMenu = () => {
+			// if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+			// 	menu.style.transform = `translate(0)`;
+			// } else {
+			// 	menu.style.transform = `translate(-100%)`;
+			// }
+			menu.classList.toggle(`active-menu`);
+		};
+
+
+		btnMenu.addEventListener(`click`, handlerMenu);
+		closeBtn.addEventListener(`click`, handlerMenu);
+		menuItems.forEach(elem => {
+			elem.addEventListener(`click`, handlerMenu);
+		});
+	};
+	toggleMenu();
+
+	// popup
+	const togglePopUp = () => {
+		const popup = document.querySelector(`.popup`),
+			popupContent = document.querySelector(`.popup-content`),
+			popupBtn = document.querySelectorAll(`.popup-btn`),
+			popupClose = document.querySelector(`.popup-close`);
+
+		let animateElem = false;
+		const animatePopup = () => {
+			let animateInterval;
+			let count = 0;
+			let animate = () => {
+				animateInterval = requestAnimationFrame(animate);
+				count++;
+				if (count <= (document.documentElement.clientHeight / 20)) {
+					popupContent.style.top = `${count * 2}px`;
+				} else {
+					cancelAnimationFrame(animateInterval);
+				}
+			}
+			animateInterval = requestAnimationFrame(animate);
+
+			if (screen.width > 768 && (!animateElem)) {
+				popup.style.display = `block`;
+				animateInterval = requestAnimationFrame(animate);
+				animateElem = true;
+			} else if (screen.width < 768) {
+				cancelAnimationFrame(animateInterval);
+				popup.style.display = `block`;
+				popupClose.addEventListener(`click`, () => {
+					popup.style.display = `none`;
+				});
+			} else {
+				animateElem = false;
+				cancelAnimationFrame(animateInterval);
+				popup.style.display = `none`;
+			}
+
+		};
+
+		popupBtn.forEach((elem) => {
+			elem.addEventListener(`click`, animatePopup);
+		});
+		popupClose.addEventListener(`click`, animatePopup);
+	};
+	togglePopUp();
 
 });
